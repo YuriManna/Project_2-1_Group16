4c57b5892a5d55d17f7e928556d5a95b19a4e7f0
Yuri Manna <115259520+YuriManna@users.noreply.github.com>
2023-09-14 18:16:34 +0200

    pieces and board base

---
+++ src/main/java/com/quarto/setup/Board.java
@@ -0,0 +1,11 @@
+package com.quarto.setup;
+
+public class Board {
+    private final int ROWS = 4;
+    private final int COLS = 4;
+    private final Pieces[][] Board = new Pieces[ROWS][COLS];
+
+    public Board() {
+
+    }
+}
---
+++ src/main/java/com/quarto/setup/Pieces.java
@@ -0,0 +1,15 @@
+package com.quarto.setup;
+
+public class Pieces {
+private boolean Color;
+private boolean Height;
+private boolean Hole;
+private boolean Shape;
+
+    public Pieces(boolean color, boolean height, boolean hole, boolean shape) {
+        Color = color;
+        Height = height;
+        Hole = hole;
+        Shape = shape;
+    }
+}
