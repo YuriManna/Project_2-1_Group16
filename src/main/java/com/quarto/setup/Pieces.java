c21659774c0b6ea74acf3e530e59e06225082a7e
MPukis <91744924+MPukis@users.noreply.github.com>
2023-10-04 12:10:33 +0200

    Win condition rules

--- src/main/java/com/quarto/setup/Pieces.java
+++ src/main/java/com/quarto/setup/Pieces.java
@@ -5,11 +5,14 @@ private boolean Color;
 private boolean Height;
 private boolean Hole;
 private boolean Shape;
+public boolean[] Properties;
 
     public Pieces(boolean color, boolean height, boolean shape, boolean hole) {
         Color = color;
         Height = height;
         Shape = shape;
         Hole = hole;
+        Properties= new boolean[]{color, height, shape, hole};
     }
+
 }
