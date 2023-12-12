e23e22ef3c422e4dec76e91b170e369d502b6fe3
mzbos1 <m.zbos@student.maastrichtuniversity.nl>
2023-12-12 22:03:12 +0100

    pvcpu button launches minimax

--- src/main/java/com/quarto/PlayMinimax.java
+++ src/main/java/com/quarto/PlayMinimax.java
@@ -2,7 +2,7 @@ package com.quarto;
 
 import com.quarto.Controller.GameController;
 
-public class Play {
+public class PlayMinimax {
     public static void main(String[] args) {
         GameController start = new GameController();
     }
