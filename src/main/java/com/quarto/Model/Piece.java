979708c3b707feaf90ef50f4c1ecf303ea191ffb
MPukis <91744924+MPukis@users.noreply.github.com>
2023-12-12 15:21:13 +0100

    now you can win

--- src/main/java/com/quarto/Model/Piece.java
+++ src/main/java/com/quarto/Model/Piece.java
@@ -39,4 +39,33 @@ public class Piece {
     public void setPiece(String piece) {
 
     }
+    public boolean[] getProperties(){return Properties;}
+
+//    public static void main(String[] args) {
+//        int count =0;
+//        Piece p1=new Piece(true,true,false,false);
+//        Piece p2 =new Piece(false,true,true,true);
+//        Piece p3 =new Piece(false,true,false,true);
+//        Piece p4 =new Piece(true,true,false,true);
+//        boolean[][] checkArr= {p1.getProperties(),p2.getProperties(),p3.getProperties(),p4.getProperties()};
+//        for (int i = 0; i <checkArr.length; i++) {
+//            for (int j = 0; j < checkArr.length; j++) {
+//                System.out.print(checkArr[i][j]+ " " );
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 0; i < checkArr.length; i++) {
+//            count++;
+//            for (int j = 0; j < checkArr.length; j++) {
+//                if(j!=3&&checkArr[j][i]==checkArr[j+1][i]){
+//                    count++;
+//                    System.out.println(count);
+//                    if(count==4){}
+//                }
+//
+//            }
+//            System.out.println("next collumn");
+//            count=0;
+//        }
+//    }
 }
