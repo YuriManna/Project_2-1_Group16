0b2dfad8a6c447490ebccd1547d861314357dfcd
mzbos1 <m.zbos@student.maastrichtuniversity.nl>
2023-12-12 23:02:37 +0100

    choosepiece

--- src/main/java/com/quarto/View/Console/ConsoleGame.java
+++ src/main/java/com/quarto/View/Console/ConsoleGame.java
@@ -129,10 +129,6 @@ boolean AITurn;
         return chosenpiece;
     }
 
-
-
-
-
     public Piece AIChoosePiece(Player opponent){
         System.out.println("AI is choosing a piece to give to the opponent:");
         showPlayerPieces(opponent);
@@ -145,12 +141,17 @@ boolean AITurn;
         return chosenpiece;
     }
 
+    // Choose a piece to give to the opponent
     public Piece choosePiece(Player opponent){
-        Piece chosenpiece = null;
+
+        System.out.println("Please choose a piece to give to your opponent:");
+        showPlayerPieces(opponent);
+
+        Piece chosenpiece = opponent.getAvailablePieces()[sc.nextInt()-1];
+        opponent.removeAvailablePiece(chosenpiece);
+
         return chosenpiece;
     }
-
-
 
 
     // Make a move
@@ -205,7 +206,5 @@ boolean AITurn;
             System.out.println("Black player wins!");
         }
     }
-
-
 
 }
